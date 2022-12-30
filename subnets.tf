d79e584104de227879776ab51a39011236eed6ee
Raghu K <raghuk.vit@gmail.com>
2022-12-30 09:06:28 +0530

    Update tf-modules/tf-module-vpc/

---
+++ subnets.tf
@@ -0,0 +1,9 @@
+resource "aws_subnet" "subnets" {
+  count      = length(var.subnet_cidr_block)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.subnet_cidr_block, count.index)
+
+  tags = {
+    Name = "${var.env}-subnet-${count.index}"
+  }
+}
