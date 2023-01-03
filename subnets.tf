63aef62ad690f2beb59cde00e700e9b15a63384e
Raghu K <raghuk.vit@gmail.com>
2023-01-03 08:27:25 +0530

    Update tf-modules/tf-module-vpc/

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,8 @@
 resource "aws_subnet" "public_subnets" {
-  count      = length(var.public_subnet_cidr_block)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.public_subnet_cidr_block, count.index)
+  count             = length(var.public_subnet_cidr_block)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnet_cidr_block, count.index)
+  availability_zone = element(var.subnet_azs, count.index)
 
   tags = {
     Name = "${var.env}-public-${count.index + 1}"
@@ -9,9 +10,10 @@ resource "aws_subnet" "public_subnets" {
 }
 
 resource "aws_subnet" "app_subnets" {
-  count      = length(var.app_subnet_cidr_block)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.app_subnet_cidr_block, count.index)
+  count             = length(var.app_subnet_cidr_block)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.app_subnet_cidr_block, count.index)
+  availability_zone = element(var.subnet_azs, count.index)
 
   tags = {
     Name = "${var.env}-app-${count.index + 1}"
@@ -19,9 +21,10 @@ resource "aws_subnet" "app_subnets" {
 }
 
 resource "aws_subnet" "db_subnets" {
-  count      = length(var.db_subnet_cidr_block)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.db_subnet_cidr_block, count.index)
+  count             = length(var.db_subnet_cidr_block)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.db_subnet_cidr_block, count.index)
+  availability_zone = element(var.subnet_azs, count.index)
 
   tags = {
     Name = "${var.env}-db-${count.index + 1}"
