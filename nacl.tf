8547d8ba88efded6448d7f15cfecc62d3aeb9af2
Raghu K <raghuk.vit@gmail.com>
2023-01-03 08:46:02 +0530

    Update tf-modules/tf-module-vpc/

--- nacl.tf
+++ nacl.tf
@@ -6,11 +6,23 @@ resource "aws_network_acl" "app" {
   }
 }
 
+resource "aws_network_acl_association" "app" {
+  count          = length(aws_subnet.app_subnets.*.id)
+  network_acl_id = aws_network_acl.app.id
+  subnet_id      = element(aws_subnet.app_subnets.*.id, count.index)
+}
+
 resource "aws_network_acl" "db" {
   vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "db"
   }
+}
+
+resource "aws_network_acl_association" "db" {
+  count          = length(aws_subnet.db_subnets.*.id)
+  network_acl_id = aws_network_acl.db.id
+  subnet_id      = element(aws_subnet.db_subnets.*.id, count.index)
 }
 
