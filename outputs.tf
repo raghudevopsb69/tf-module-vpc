1d7631b28682caa9a29e1e0e91a0dece26b10585
Raghu K <raghuk.vit@gmail.com>
2023-01-04 08:16:33 +0530

    Update tf-modules/tf-module-vpc/

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
+output "app_subnets_ids" {
+  value = aws_subnet.app_subnets.*.id
+}
+
+output "app_subnets_cidr" {
+  value = aws_subnet.app_subnets.*.cidr_block
+}
+
