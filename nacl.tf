87cfe874587ba3d1823617af9a2b31142a870bfe
Raghu K <raghuk.vit@gmail.com>
2023-01-24 09:00:15 +0530

    Update tf-modules/tf-module-vpc/

--- nacl.tf
+++ nacl.tf
@@ -1,81 +1,81 @@
-resource "aws_network_acl" "app" {
-  vpc_id = aws_vpc.main.id
-
-  egress {
-    protocol   = "-1"
-    rule_no    = 1
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 32765
-    action     = "allow"
-    cidr_block = var.workstation_ip
-    from_port  = 22
-    to_port    = 22
-  }
-
-  ingress {
-    protocol   = "-1"
-    rule_no    = 32766
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  tags = {
-    Name = "app"
-  }
-}
-
-resource "null_resource" "sleep" {
-  provisioner "local-exec" {
-    command = "sleep 60"
-  }
-}
-
-resource "aws_network_acl_association" "app" {
-  depends_on     = [null_resource.sleep]
-  count          = length(aws_subnet.app_subnets.*.id)
-  network_acl_id = aws_network_acl.app.id
-  subnet_id      = element(aws_subnet.app_subnets.*.id, count.index)
-}
-
-resource "aws_network_acl" "db" {
-  vpc_id = aws_vpc.main.id
-
-  egress {
-    protocol   = "-1"
-    rule_no    = 1
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  ingress {
-    protocol   = "-1"
-    rule_no    = 32766
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  tags = {
-    Name = "db"
-  }
-}
-
-resource "aws_network_acl_association" "db" {
-  depends_on     = [null_resource.sleep]
-  count          = length(aws_subnet.db_subnets.*.id)
-  network_acl_id = aws_network_acl.db.id
-  subnet_id      = element(aws_subnet.db_subnets.*.id, count.index)
-}
-
+//resource "aws_network_acl" "app" {
+//  vpc_id = aws_vpc.main.id
+//
+//  egress {
+//    protocol   = "-1"
+//    rule_no    = 1
+//    action     = "allow"
+//    cidr_block = "0.0.0.0/0"
+//    from_port  = 0
+//    to_port    = 0
+//  }
+//
+//  ingress {
+//    protocol   = "tcp"
+//    rule_no    = 32765
+//    action     = "allow"
+//    cidr_block = var.workstation_ip
+//    from_port  = 22
+//    to_port    = 22
+//  }
+//
+//  ingress {
+//    protocol   = "-1"
+//    rule_no    = 32766
+//    action     = "allow"
+//    cidr_block = "0.0.0.0/0"
+//    from_port  = 0
+//    to_port    = 0
+//  }
+//
+//  tags = {
+//    Name = "app"
+//  }
+//}
+//
+//resource "null_resource" "sleep" {
+//  provisioner "local-exec" {
+//    command = "sleep 60"
+//  }
+//}
+//
+//resource "aws_network_acl_association" "app" {
+//  depends_on     = [null_resource.sleep]
+//  count          = length(aws_subnet.app_subnets.*.id)
+//  network_acl_id = aws_network_acl.app.id
+//  subnet_id      = element(aws_subnet.app_subnets.*.id, count.index)
+//}
+//
+//resource "aws_network_acl" "db" {
+//  vpc_id = aws_vpc.main.id
+//
+//  egress {
+//    protocol   = "-1"
+//    rule_no    = 1
+//    action     = "allow"
+//    cidr_block = "0.0.0.0/0"
+//    from_port  = 0
+//    to_port    = 0
+//  }
+//
+//  ingress {
+//    protocol   = "-1"
+//    rule_no    = 32766
+//    action     = "allow"
+//    cidr_block = "0.0.0.0/0"
+//    from_port  = 0
+//    to_port    = 0
+//  }
+//
+//  tags = {
+//    Name = "db"
+//  }
+//}
+//
+//resource "aws_network_acl_association" "db" {
+//  depends_on     = [null_resource.sleep]
+//  count          = length(aws_subnet.db_subnets.*.id)
+//  network_acl_id = aws_network_acl.db.id
+//  subnet_id      = element(aws_subnet.db_subnets.*.id, count.index)
+//}
+//
